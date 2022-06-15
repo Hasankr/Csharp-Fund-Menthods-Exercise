b3b57e7e1ff244a3c45d47573dfe0bba79063f1c
hasankr <hasankr@abv.bg>
2022-06-15 15:19:51 +0300

    added 1

--- 01. Smallest of Three Numbers/Program.cs
+++ 01. Smallest of Three Numbers/Program.cs
@@ -6,7 +6,15 @@ namespace _01._Smallest_of_Three_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(SmallesNumber(num1, num2, num3));
+        }
+
+        static int SmallesNumber(int a, int b, int c)
+        {
+            return Math.Min(a, Math.Min(b, c));
         }
     }
 }
