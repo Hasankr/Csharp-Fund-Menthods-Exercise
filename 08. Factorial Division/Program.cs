78ca22767b7dcbbecbf7204004dc0ba6eb328a9c
hasankr <hasankr@abv.bg>
2022-06-16 00:45:58 +0300

    added 8, 9 and 11

---
+++ 08. Factorial Division/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            double result = Factorial(firstNum);
+            double result2 = Factorial(secondNum);
+            Console.WriteLine($"{result / result2:f2}");
+        }
+
+        private static double Factorial(int number)
+        {
+            double result = 1;
+            while (number != 1)
+            {
+                result *= number;
+                number--;
+            }
+            return result;
+        }
+    }
+}
