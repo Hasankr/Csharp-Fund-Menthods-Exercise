7a7bae49b97ddb6f385468e8583cda55253ba32c
hasankr <hasankr@abv.bg>
2022-06-15 15:40:39 +0300

    added 2.0

---
+++ 02. Vowels Count/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._Vowels_Count
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText = Console.ReadLine().ToLower();
+            SearchForVowels(inputText);
+        }
+
+        private static void SearchForVowels(string text)
+        {
+            Console.WriteLine(text.Count(vowles => "aouei".Contains(vowles)));
+        }
+    }
+}
